a936c4969864d1d2a55f9c4dfc7af27ecb293305
Young Jun Yun <yy245@duke.edu>
2019-11-05 09:51:18 -0500

    First version

---
+++ python-fasta.py
@@ -0,0 +1,13 @@
+def read_fasta(filename):
+    """
+    Read in a file in FASTA format
+    """
+    seq = ''
+    f = open(filename)
+    for line in f:
+        line = line.strip()
+        if not '>' in line:
+            seq = seq + line #only keep the line if it's not an identifier line that begins with '>'
+    f.close()
+    return seq
+print(read_fasta('ae.fa'))
